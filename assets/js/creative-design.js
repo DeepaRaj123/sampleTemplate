5ae7924609c283b1534b4bf10c7c0ee29a0fecb3
DeepaRaj123 <deeparaj096@gmail.com>
2022-08-27 18:15:50 +0530

    add files

--- assets/js/creative-design.js
+++ assets/js/creative-design.js
@@ -101,3 +101,18 @@ $.each( instance, function(key, value) {
   }
   
 });
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("header");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
