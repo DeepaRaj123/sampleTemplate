b0723bbf8bdc9c837440e60fcbf1cc99233355f3
DeepaRaj123 <deeparaj096@gmail.com>
2022-08-27 17:27:01 +0530

    add files

--- assets/js/creative-design.js
+++ assets/js/creative-design.js
@@ -35,42 +35,69 @@ window.addEventListener("load", function () {
 	document.getElementById("loader").style.display = "none";
 	document.getElementById("box").style.display = "block";
 });
-const testimonials = [
-	{
-		name: "Vivek Mahajan",
-		job: "Larimars Clothing Toronto,Canada",
-		image: "./assets/imgs/avatar1.png",
-		testimonial:
-			"The team at Mindfully sustainable is very well versed and helped setting up systems to achieve our sustainable goals. We got on board with the team to help our small apparel manufacturing setup to reduce its carbon foot print and reduce wastage so it can either be repurposed or upscaled. We have seen a significant change in how small but very useful techniques when implemented correctly can contribute to the greater cause of value addition, helps being compliant with circular ideology and putting positive contribution to the community. Thank you again for your help and would highly recommend using their services."
-	},
-	{
-		name: "Farah Alam",
-		job: "QA/Test Engineer Dartford,UK",
-		image: "./assets/imgs/avatar1.png",
-		testimonial:
-			"Anamika is an amazing consultant and a thorough professional. She has helped and guided us through the process, to becoming completely sustainable in terms and f our food habits, lifestyle and fashion. We can feel the difference it has made in the quality of our life. It has become healthy, enjoyable and stress & guilt free."
-	}
-];
-let i = 0; // current slide
-let j = testimonials.length; // total slides
-let testimonialContainer = document.getElementById("testimonial-container");
-function next() {
-	i = (j + i + 1) % j;
-	displayTestimonial();
-}
-
-function prev() {
-	i = (j + i - 1) % j;
-	displayTestimonial();
-}
-let displayTestimonial = () => {
-	testimonialContainer.innerHTML = `
-        <p>${testimonials[i].testimonial}</p>
-         <img src=${testimonials[i].image}></img>
-        <h3>${testimonials[i].name}</h3>
-        <h6>${testimonials[i].job}</h6>
-        `;
-};
-window.onload = displayTestimonial;
 
 
+
+
+
+// work in progress - needs some refactoring and will drop JQuery i promise :)
+var instance = $(".hs__wrapper");
+$.each( instance, function(key, value) {
+    
+  var arrows = $(instance[key]).find(".arrow"),
+      prevArrow = arrows.filter('.arrow-prev'),
+      nextArrow = arrows.filter('.arrow-next'),
+      box = $(instance[key]).find(".hs"), 
+      x = 0,
+      mx = 0,
+      maxScrollWidth = box[0].scrollWidth - (box[0].clientWidth / 2) - (box.width() / 2);
+
+  $(arrows).on('click', function() {
+      
+    if ($(this).hasClass("arrow-next")) {
+      x = ((box.width() / 2)) + box.scrollLeft() - 10;
+      box.animate({
+        scrollLeft: x,
+      })
+    } else {
+      x = ((box.width() / 2)) - box.scrollLeft() -10;
+      box.animate({
+        scrollLeft: -x,
+      })
+    }
+      
+  });
+    
+  $(box).on({
+    mousemove: function(e) {
+      var mx2 = e.pageX - this.offsetLeft;
+      if(mx) this.scrollLeft = this.sx + mx - mx2;
+    },
+    mousedown: function(e) {
+      this.sx = this.scrollLeft;
+      mx = e.pageX - this.offsetLeft;
+    },
+    scroll: function() {
+      toggleArrows();
+    }
+  });
+
+  $(document).on("mouseup", function(){
+    mx = 0;
+  });
+  
+  function toggleArrows() {
+    if(box.scrollLeft() > maxScrollWidth - 10) {
+        // disable next button when right end has reached 
+        nextArrow.addClass('disabled');
+      } else if(box.scrollLeft() < 10) {
+        // disable prev button when left end has reached 
+        prevArrow.addClass('disabled')
+      } else{
+        // both are enabled
+        nextArrow.removeClass('disabled');
+        prevArrow.removeClass('disabled');
+      }
+  }
+  
+});
